19428c50100e9ceae6549b828acade577aed419d
kei <entei115115@gmail.com>
2023-01-13 23:54:41 +0900

    レベルから必要経験値を算出する仕組みを追加(途中)

--- main.jsx
+++ main.jsx
@@ -1,80 +1,88 @@
 function App() {
-    const [name, setName] = React.useState([1640000, 10, 10, 100, 100, 100, "false", 10, 10, 100, 100, 100, 0]);
+    const [podate, setPodate] = React.useState([50, 10, 10, 100, 100, 100, "true", 10, 10, 100, 100, 100, 0]);
 
-    const handleChange = function(e) {
+    const xlCanChange = function(e) {
         console.log(e.target.value);
         var target = e.target.value;
         target = target > 999 ? 999 : target;
-        var tmp = calcRequirdCandy(name[0], target, name[2], name[3], name[4], name[5]);
+        var requiredEXP = POKEMON_EXPTYPE["ニャオハ"][podate[0]];
+        console.log(requiredEXP);
+        var tmp = calcRequirdCandy(podate[0], target, podate[2], podate[3], podate[4], podate[5]);
         tmp[0] = tmp[0] ? "true" : "false";
-        setName([name[0], target, name[2], name[3], name[4], name[5], tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
+        setPodate([podate[0], target, podate[2], podate[3], podate[4], podate[5], tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
     }
 
-    const handleChange2 = function(e) {
+    const lCanChange = function(e) {
         console.log(e.target.value);
         var target = e.target.value;
         target = target > 999 ? 999 : target;
-        var tmp = calcRequirdCandy(name[0], name[1], e.target.value, name[3], name[4], name[5]);
+        var requiredEXP = POKEMON_EXPTYPE["ニャオハ"][podate[0]];
+        console.log(requiredEXP);
+        var tmp = calcRequirdCandy(podate[0], podate[1], target, podate[3], podate[4], podate[5]);
         tmp[0] = tmp[0] ? "true" : "false";
-        setName([name[0], name[1], target, name[3], name[4], name[5], tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
+        setPodate([podate[0], podate[1], target, podate[3], podate[4], podate[5], tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
     }
 
-    const handleChange3 = function(e) {
+    const mCanChange = function(e) {
         console.log(e.target.value);
         var target = e.target.value;
         target = target > 999 ? 999 : target;
-        var tmp = calcRequirdCandy(name[0], name[1], name[2], e.target.value, name[4], name[5]);
+        var requiredEXP = POKEMON_EXPTYPE["ニャオハ"][podate[0]];
+        console.log(requiredEXP);
+        var tmp = calcRequirdCandy(podate[0], podate[1], podate[2], target, podate[4], podate[5]);
         tmp[0] = tmp[0] ? "true" : "false";
-        setName([name[0], name[1], name[2], target, name[4], name[5], tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
+        setPodate([podate[0], podate[1], podate[2], target, podate[4], podate[5], tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
     }
 
-    const handleChange4 = function(e) {
+    const sCanChange = function(e) {
         console.log(e.target.value);
         var target = e.target.value;
         target = target > 999 ? 999 : target;
-        var tmp = calcRequirdCandy(name[0], name[1], name[2], name[3], e.target.value, name[5]);
+        var requiredEXP = POKEMON_EXPTYPE["ニャオハ"][podate[0]];
+        console.log(requiredEXP);
+        var tmp = calcRequirdCandy(podate[0], podate[1], podate[2], podate[3], target, podate[5]);
         tmp[0] = tmp[0] ? "true" : "false";
-        setName([name[0], name[1], name[2], name[3], target, name[5], tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
+        setPodate([podate[0], podate[1], podate[2], podate[3], target, podate[5], tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
     }
 
-    const handleChange5 = function(e) {
+    const xsCanChange = function(e) {
         console.log(e.target.value);
         var target = e.target.value;
         target = target > 999 ? 999 : target;
-        var tmp = calcRequirdCandy(name[0], name[1], name[2], name[3], name[4], e.target.value);
+        var requiredEXP = POKEMON_EXPTYPE["ニャオハ"][podate[0]];
+        console.log(requiredEXP);
+        var tmp = calcRequirdCandy(podate[0], podate[1], podate[2], podate[3], podate[4], target);
         tmp[0] = tmp[0] ? "true" : "false";
-        setName([name[0], name[1], name[2], name[3], name[4], target, tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
+        setPodate([podate[0], podate[1], podate[2], podate[3], podate[4], target, tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
     }
 
-    const handleChange0 = function(e) {
+    const pokemonChange = function(e) {
         console.log(e.target.value);
-        var tmp = e.target.value | 0;
-        setName([tmp, name[1], name[2], name[3], name[4], name[5]]);
+        console.log(TYPE100[1]);
+        var target = e.target.value;
+        target = target > 100 ? 100 : target;
+        var requiredEXP = POKEMON_EXPTYPE["ニャオハ"][target];
+        console.log(requiredEXP);
+        var tmp = calcRequirdCandy(requiredEXP, podate[1], podate[2], podate[3], podate[4], podate[5]);
+        tmp[0] = tmp[0] ? "true" : "false";
+        setPodate([target, podate[1], podate[2], podate[3], podate[4], podate[5], tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]);
     }
-
-    //e => setName(TestFunc(e.target.value | 0,1) | 0);
-    const handleClick = _ => setName([5,10,15]);
 
     return (
         <div>
-            <h1>{name[6]}, {name[12]}</h1>
-            re<input value={name[0]} onChange={handleChange0}/><br></br>
-            xl<input value={name[1]} onChange={handleChange}/><br></br>
-            l<input value={name[2]} onChange={handleChange2}/><br></br>
-            m<input value={name[3]} onChange={handleChange3}/><br></br>
-            s<input value={name[4]} onChange={handleChange4}/><br></br>
-            xs<input value={name[5]} onChange={handleChange5}/><br></br>
-            <input type="button" onClick={handleClick}/>
+            <h1>{podate[6]}, {podate[12]}</h1>
+            start<input value={podate[0]} onChange={pokemonChange}/><br></br>
+            xl<input value={podate[1]} onChange={xlCanChange}/><br></br>
+            l<input value={podate[2]} onChange={lCanChange}/><br></br>
+            m<input value={podate[3]} onChange={mCanChange}/><br></br>
+            s<input value={podate[4]} onChange={sCanChange}/><br></br>
+            xs<input value={podate[5]} onChange={xsCanChange}/><br></br>
         </div>
     );
 }
-
+//end作らなケラバならぬ　end - start = 必要経験値
 const target = document.querySelector('#app');
 ReactDOM.render(<App />, target);
-
-let TestFunc = function(i,j){
-    return (i | 0) + (j | 0);
-}
 
 let calcRequirdCandy = function (requiredEXP, xlLimit, lLimit, mLimit, sLimit, xsLimit) {
     var xls = 0;
@@ -84,33 +92,32 @@ let calcRequirdCandy = function (requiredEXP, xlLimit, lLimit, mLimit, sLimit, x
     var xss = 0;
 
     for (let i = 0; i <= xsLimit; i = (i + 1) | 0) {
-        console.log(1);
         xss = i;
-        console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
+        //console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
         if (requiredEXP <= i * 100) { break; }
     }
 
     for (let i = 0; i <= sLimit; i = (i + 1) | 0) {
         ss = i;
-        console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
+        //console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
         if (requiredEXP <= i * 800 + xss * 100) { break; }
     }
 
     for (let i = 0; i <= mLimit; i = (i + 1) | 0) {
         ms = i;
-        console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
+        //console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
         if (requiredEXP <= i * 3000 + ss * 800 + xss * 100) { break; }
     }
 
     for (let i = 0; i <= lLimit; i = (i + 1) | 0) {
         ls = i;
-        console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
+        //console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
         if (requiredEXP <= i * 10000 + ms * 3000 + ss * 800 + xss * 100) { break; }
     }
 
     for (let i = 0; i <= xlLimit; i = (i + 1) | 0) {
         xls = i;
-        console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
+        //console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
         if (requiredEXP <= i * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100) { break; }
     }
 
@@ -126,31 +133,31 @@ let calcRequirdCandy = function (requiredEXP, xlLimit, lLimit, mLimit, sLimit, x
         console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100 - requiredEXP);
 
         for (let i = xls; i > 0; i = (i - 1) | 0) {
-            console.log(i * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
+            //console.log(i * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100)
             if (requiredEXP > i * 30000 + ls * 10000 + ms * 3000 + ss * 800 + xss * 100) { break; }
             xls = i;
         }
 
         for (let i = ls; i > 0; i = (i - 1) | 0) {
-            console.log(xls * 30000 + i * 10000 + ms * 3000 + ss * 800 + xss * 100)
+            //console.log(xls * 30000 + i * 10000 + ms * 3000 + ss * 800 + xss * 100)
             if (requiredEXP > xls * 30000 + i * 10000 + ms * 3000 + ss * 800 + xss * 100) { break; }
             ls = i;
         }
 
         for (let i = ms; i > 0; i = (i - 1) | 0) {
-            console.log(xls * 30000 + ls * 10000 + i * 3000 + ss * 800 + xss * 100)
+            //console.log(xls * 30000 + ls * 10000 + i * 3000 + ss * 800 + xss * 100)
             if (requiredEXP > xls * 30000 + ls * 10000 + i * 3000 + ss * 800 + xss * 100) { break; }
             ms = i;
         }
 
         for (let i = ss; i > 0; i = (i - 1) | 0) {
-            console.log(xls * 30000 + ls * 10000 + ms * 3000 + i * 800 + xss * 100)
+            //console.log(xls * 30000 + ls * 10000 + ms * 3000 + i * 800 + xss * 100)
             if (requiredEXP > xls * 30000 + ls * 10000 + ms * 3000 + i * 800 + xss * 100) { break; }
             ss = i;
         }
 
         for (let i = xss; i > 0; i = (i - 1) | 0) {
-            console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + i * 100)
+            //console.log(xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + i * 100)
             if (requiredEXP > xls * 30000 + ls * 10000 + ms * 3000 + ss * 800 + i * 100) { break; }
             xss = i;
         }
